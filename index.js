a5f93a9f636632d441f95bc26a3c433553f10769
Uriel Chemouni <uriel.chemouni@gmail.com>
2023-04-12 03:24:55 +0000

    first version  to test.

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const core = require('@actions/core');
+const cache = require('@actions/cache');
+const exec = require('@actions/exec');
 const wait = require('./wait');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    let cacheKey = undefined;
+    const branch = core.getInput('branch');
+    if (!cache.isFeatureAvailable) {
+      core.setOutput("Cache service is not availible");
+    } else {
+      const platform = process.env.RUNNER_OS;
+      cacheKey = cache.restoreCache(["opencv"], `opencv-${platform}-${branch}`);
+    }
+    if (cacheKey) {
+      // done for now.
+      core.setOutput("Cache Restored");
+      return;
+    }
+    core.info(`Calling actions/checkout ...`);
+    await exec.exec("actions/checkout@v2", ['--branch', branch, '--repository', 'opencv/opencv', '--path', 'opencv']);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
+    core.setOutput('getExecOutput("ls -l")');
+    await exec.getExecOutput("ls -l");
+    core.setOutput('exec("ls -l")');
+    await  exec.exec("ls -l");
+
+    // await wait(parseInt(ms));
     core.info((new Date()).toTimeString());
-
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
