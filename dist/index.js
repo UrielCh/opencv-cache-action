2b64ff0847c8a468310629f3ef63a38cc3686c0c
UrielCh <uriel.chemouni@gmail.com>
2023-04-12 10:38:06 +0300

    build and cache

--- dist/index.js
+++ dist/index.js
@@ -57963,6 +57963,7 @@ const process = __nccwpck_require__(7282);
 // most @actions toolkit packages have async methods
 async function run() {
   try {
+    const cachePaths = ["opencv", "opencv_contrib", "build"];
     await exec.exec("pwd");
     // get one LVL up
     process.chdir('..');
@@ -57970,18 +57971,15 @@ async function run() {
     let cacheKey = undefined;
     const branch = core.getInput('branch');
     const platform = process.env.RUNNER_OS;
-    const storeKey = `opencv-${platform}-${branch}-test`;
-
+    const storeKey = `opencv-${platform}-${branch}`;
     console.time('cache');
-
-
     if (!cache.isFeatureAvailable) {
       console.log("NO CACHE");
       core.setOutput("Cache service is not availible");
     } else {
       console.log("Get Cache:");
       console.log(`cahce key: opencv-${platform}-${branch}`);
-      cacheKey = await cache.restoreCache(["opencv"], storeKey);
+      cacheKey = await cache.restoreCache(cachePaths, storeKey);
       console.log(`cacheKey:${cacheKey}`);
     }
 
@@ -57992,22 +57990,22 @@ async function run() {
       return;
     }
 
-    await exec.exec("git", ['clone', '--branch', branch, '--single-branch', '--depth', '1', 'https://github.com/opencv/opencv.git', 'opencv']);
-    //await exec.exec("git", ['clone', '--branch', branch, '--single-branch', '--depth', '1', 'https://github.com/opencv/opencv_contrib.git', 'opencv_contrib']);
+    await exec.exec("git", ['clone', '--quiet', '--branch', branch, '--single-branch', '--depth', '1', 'https://github.com/opencv/opencv.git', 'opencv']);
+    await exec.exec("git", ['clone', '--branch', branch, '--single-branch', '--depth', '1', 'https://github.com/opencv/opencv_contrib.git', 'opencv_contrib']);
     await io.mkdirP('build');
     process.chdir('build');
     // see doc: https://docs.opencv.org/4.x/db/d05/tutorial_config_reference.html
-    //await exec.exec("cmake", [
-    //  '-DCMAKE_BUILD_TYPE=Release',
-    //  '-DOPENCV_ENABLE_NONFREE=ON',
-    //  '-DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules',
-    //  '-DBUILD_LIST=core,imgcodecs,calib3d,python3,python_bindings_generator',
-    //  '../opencv',
-    //]);
-    // await exec.exec("cmake", [ '--build', '.']);
+    await exec.exec("cmake", [
+      '-DCMAKE_BUILD_TYPE=Release',
+      '-DOPENCV_ENABLE_NONFREE=ON',
+      '-DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules',
+      '-DBUILD_LIST=core,imgcodecs,calib3d,python3,python_bindings_generator',
+      '../opencv',
+    ]);
+    await exec.exec("cmake", [ '--build', '.']);
     process.chdir('..');
     await exec.exec("ls -l");
-    const ret = await cache.saveCache(["opencv"], storeKey);
+    const ret = await cache.saveCache(cachePaths, storeKey);
     // await wait(parseInt(ms));
     console.log('saveCache return ', ret);
     // core.setOutput('time', new Date().toTimeString());
